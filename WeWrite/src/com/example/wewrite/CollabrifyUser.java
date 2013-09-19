d15ec2cc7c266edf4b0090c6df674a4367791371
David Rushdoony <drushdoony@gmail.com>
2013-09-19 19:23:44 -0400

    Create CollabrifyUser.java

---
+++ WeWrite/src/com/example/wewrite/CollabrifyUser.java
@@ -0,0 +1,27 @@
+package com.example.wewrite;
+
+import java.util.Stack;
+
+public class CollabrifyUser
+{
+  private Stack<UserAction> pastActions;
+  private Stack<UserAction> undoneActions;
+  
+  public CollabrifyUser() {    
+  }
+  
+  public void addPastAction( UserAction action ) {
+    pastActions.push(action);
+  }
+  
+  public UserAction undoPastAction() {
+    undoneActions.push(pastActions.peek());
+    return pastActions.pop();
+  }
+  
+  public UserAction redoUndoneAction() {
+    pastActions.push(undoneActions.peek());
+    return undoneActions.pop();
+  }
+
+}
