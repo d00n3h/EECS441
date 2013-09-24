08199e7886b0d925be6dec4e10e9deda20888a57
d00n3h <drushdoony@gmail.com>
2013-09-23 21:06:58 -0400

    Added WeWriteCollabrifyListener

---
+++ WeWrite/src/com/example/wewrite/WeWriteCollabrifyListener.java
@@ -0,0 +1,98 @@
+package com.example.wewrite;
+
+import java.util.List;
+
+import edu.umich.imlc.collabrify.client.CollabrifyListener;
+import edu.umich.imlc.collabrify.client.CollabrifyParticipant;
+import edu.umich.imlc.collabrify.client.CollabrifySession;
+import edu.umich.imlc.collabrify.client.exceptions.CollabrifyException;
+
+public class WeWriteCollabrifyListener implements CollabrifyListener
+{
+
+  @Override
+  public void onSessionCreated(long id)
+  {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public byte[] onBaseFileChunkRequested(long currentBaseFileSize)
+  {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public void onBaseFileUploadComplete(long baseFileSize)
+  {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void onSessionJoined(long maxOrderId, long baseFileSize)
+  {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void onBaseFileChunkReceived(byte[] baseFileChunk)
+  {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void onDisconnect()
+  {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void onReceiveEvent(long orderId, int submissionRegistrationId,
+      String eventType, byte[] data)
+  {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void onReceiveSessionList(List<CollabrifySession> sessionList)
+  {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void onParticipantJoined(CollabrifyParticipant p)
+  {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void onParticipantLeft(CollabrifyParticipant p)
+  {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void onError(CollabrifyException e)
+  {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void onSessionEnd(long id)
+  {
+    // TODO Auto-generated method stub
+    
+  }
+
+}
